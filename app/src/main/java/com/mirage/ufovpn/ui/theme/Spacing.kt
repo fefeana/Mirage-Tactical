ac891b5ab191609ac8c91de582b9f33f09b87600
fefeana <alfacc222@gmail.com>
2026-07-17 17:34:50 +0300

    Add SamuraiSpacing object for UI spacing constants

---
+++ app/src/main/java/com/mirage/ufovpn/ui/theme/Spacing.kt
@@ -0,0 +1,24 @@
+package com.mirage.ufovpn.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object SamuraiSpacing {
+    val none = 0.dp
+    val tiny = 4.dp
+    val small = 8.dp
+    val medium = 16.dp
+    val large = 24.dp
+    val xLarge = 32.dp
+    val huge = 48.dp
+    val massive = 64.dp
+
+    val buttonHeight = 56.dp
+    val iconSize = 24.dp
+    val iconLarge = 32.dp
+    val logoSize = 80.dp
+    val cardRadius = 16.dp
+    val borderWidth = 2.dp
+
+    val screenPadding = 24.dp
+    val cardPadding = 16.dp
+}
