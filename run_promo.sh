56e16f9c05f68b84ce7391bf3d34f3a6659dfcdf
fefeana <alfacc222@gmail.com>
2026-07-22 23:57:37 +0300

    Fix installation command in run_promo.sh

--- run_promo.sh
+++ run_promo.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-
-       -y ffmpeg python3-pip
-
+sudo apt-get update
+sudo apt-get install -y ffmpeg python3-pip
 pip install pillow moviepy numpy gTTS
-
 python3 video_generator.py
