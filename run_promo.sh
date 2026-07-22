b2e5bdf49551ed34a661404fa41f7280a2e3f485
fefeana <alfacc222@gmail.com>
2026-07-23 00:11:19 +0300

    Format package installation commands in run_promo.sh
    Refactor installation commands for better readability.

--- run_promo.sh
+++ run_promo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
-
 sudo apt-get update
-sudo apt-get install -y ffmpeg python3-pip
-pip install pillow moviepy numpy gTTS
+sudo apt-get install -y \
+  ffmpeg python3-pip
+
+pip install \
+  pillow moviepy numpy gTTS
+
 python3 video_generator.py
