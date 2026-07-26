47891455275080e0caddbbada899d276ff6ed558
fefeana <alfacc222@gmail.com>
2026-07-27 00:31:30 +0300

    Add singleton AnalyticsService class

---
+++ lib/services/analytics_service.dart
@@ -0,0 +1,6 @@
+class AnalyticsService {
+  static final AnalyticsService _instance = AnalyticsService._internal();
+  factory AnalyticsService() => _instance;
+  AnalyticsService._internal();
+  Future<void> initialize() async {}
+}
