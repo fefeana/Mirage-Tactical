fb1dea73df51aebeac3ca74d08ef2580b7e92f5e
fefeana <alfacc222@gmail.com>
2026-07-27 00:28:26 +0300

    Add DownloadScreen widget for downloads display

---
+++ lib/screens/download_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DownloadScreen extends StatelessWidget {
+  const DownloadScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('Downloads')));
+  }
+}
